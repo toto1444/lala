19afa4f70cc3070e4230aee731bcf329e294d6cc
AhHyeon An <ahhyeon@localhost.localdomain>
2017-11-29 13:56:36 +0900

    [Changed] Start_newWorld

---
+++ app/src/main/java/com/yee/myapplication/AppCompatActivity.java
@@ -0,0 +1,11 @@
+package com.yee.myapplication;
+
+import android.os.Bundle;
+
+/**
+ * Created by ahhye on 11/23/2017.
+ */
+public class AppCompatActivity {
+    public void onCreate(Bundle savedInstanceState) {
+    }
+}
--- app/src/main/java/com/yee/myapplication/MainActivity.java
+++ app/src/main/java/com/yee/myapplication/MainActivity.java
@@ -1,53 +1,56 @@
 package com.yee.myapplication;
 
-import android.support.annotation.NonNull;
-import android.support.design.internal.BottomNavigationItemView;
-import android.support.design.widget.BottomNavigationView;
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.Window;
-import android.widget.FrameLayout;
-import android.widget.Toast;
+import android.widget.ImageView;c
+
+import com.pixplicity.sharp.Sharp;
+//import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView mImageView;
+    private Sharp mSvg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.navbot);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.navbot);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                if(item.getItemId() == R.id.subway){
-                    //do something
-                    Toast.makeText(MainActivity.this, "Ahyeon", Toast.LENGTH_SHORT).show();
+                android.support.v4.app.Fragment selectedFragment = null;
+                switch (item.getItemId()) {
+                    case R.id.subway:
+                        selectedFragment = ItemOneFragment.newInstance();
+                        break;
+                    case R.id.bus:
+                        selectedFragment = ItemTwoFragment.newInstance();
+                        break;
+                    /*case R.id.action_item3:
+                        selectedFragment = ItemThreeFragment.newInstance();
+                        break;*/
                 }
-
-                else if(item.getItemId() == R.id.bus){
-                    //do something
-                    Toast.makeText(MainActivity.this, "Ahhyeon", Toast.LENGTH_SHORT).show();
-                }
-
-                else if(item.getItemId() == R.id.taxi){
-                    //do something
-                    Toast.makeText(MainActivity.this, "Ahhhyeon", Toast.LENGTH_SHORT).show();
-                }
-
-                else if(item.getItemId() == R.id.food){
-                    //do something
-                    Toast.makeText(MainActivity.this, "Ahhhhyeon", Toast.LENGTH_SHORT).show();
-                }
-
-                else if(item.getItemId() == R.id.emergency){
-                    //do something
-                    Toast.makeText(MainActivity.this, "Ahhhhhyeon", Toast.LENGTH_SHORT).show();
-                }
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.frame_layout, selectedFragment);
+                transaction.commit();
                 return true;
             }
         });
+        //Manually displaying the first fragment - one time only
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_layout, ItemOneFragment.newInstance());
+        transaction.commit();
+
+        //Used to select an item programmatically
+        //bottomNavigationView.getMenu().getItem(2).setChecked(true);
     }
 }
